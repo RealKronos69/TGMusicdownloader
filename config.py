5e9837926a4815bc4274c72cbe0ca0c2590ed795
RealKronos69 <138235386+RealKronos69@users.noreply.github.com>
2023-10-13 12:50:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-# © Cyril C Thomas
-# https://t.me/cyril_c_10
+# © KronoS
+# https://t.me/fakekronos
 
 import os
 
@@ -7,7 +7,7 @@ class Config(object):
     BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
     API_ID = int(os.environ.get("APP_ID", ))
     API_HASH = os.environ.get("API_HASH", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001651192249))
     MONGODB_URI = os.environ.get("MONGODB_URI", "")
     SESSION_NAME = os.environ.get("SESSION_NAME", "")
     BOT_OWNER = int(os.environ.get("BOT_OWNER", ))
